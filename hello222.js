656d2e578a4a3344e336fe8e5c38870ad9bea7a5
JoHimChan <nimi1512@naver.com>
2021-06-09 16:39:19 +0900

    aa

--- hello222.js
+++ hello222.js
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
+onsole.log("------>");
 console.log("------>");
 console.log("----...");
 console.log("test6");
 console.log("test7");
-=======
 console.log("test4");
 console.log("test5");
 console.log("=====");
 console.log("test7");
 console.log("test8");
->>>>>>> 1436c5ac22ce2b5bf9da376c70730bfb62b3464a
+
