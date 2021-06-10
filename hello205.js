8eb367b025b338f4fefdcf240b9404000d9d3193
JoHimChan <JoHimChan@github.com>
2021-06-10 10:54:06 +0900

    d

--- hello205.js
+++
@@ -1,5 +0,0 @@
-console.log("test1");
-console.log("test2");
-console.log("tessdfsdfsdssdf");
-console.log("tessdfsdfsdfsdfsdf");
-console.log("tsdfsdfsdfsdf");
--- hello3.js
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-console.log("Hello, world!");
-=======
-console.log("Hello, world!");
-console.log("hi");
->>>>>>> 334a08c1c17566af286aec475056febda1bc37e1
